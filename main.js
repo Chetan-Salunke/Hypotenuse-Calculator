5df2870641a694d2e4778c45e5b9ed19ed1972eb
chetan <chetansalunke019317@gmail.com>
2023-04-10 17:57:32 +0530

    Formating

--- main.js
+++ main.js
@@ -2,23 +2,13 @@ let a;
 let b;
 let c;
 
-// a = window.prompt("enter the value of A")
-// a = Number(a);
-
-// b = window.prompt("enter the value of B")
-// b = Number(b);
-
-// c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-
-// console.log("Side c is : ", c);
-
 
 document.getElementById("submitbutton").onclick = function () {
 
-    a = document.getElementById("texta").value;
+    a = document.getElementById("Texta").value;
     a = Number(a);
 
-    b = document.getElementById("textb").value;
+    b = document.getElementById("Textb").value;
     b = Number(b);
 
     c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
